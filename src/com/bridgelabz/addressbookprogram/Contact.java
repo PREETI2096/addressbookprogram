873c9da4005457edf0b10e2d7010d1415fdde205
PREETI2096 <preetirg96@gmail.com>
2023-01-23 15:04:25 +0530

    Added : Address book program

--- src/com/bridgelabz/addressbookprogram/Contact.java
+++ src/com/bridgelabz/addressbookprogram/Contact.java
@@ -1,14 +1,15 @@
 package com.bridgelabz.addressbookprogram;
 
 public class Contact {
-	String firstName;
-	String lastName;
-	String address;
-	long  phoneNumber;
-	String emailID;
-	String city;
-	String state;
-	long  zip;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private long  phoneNumber;
+	private String emailID;
+	private String city;
+	private String state;
+	private long  zip;
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -59,7 +60,7 @@ public class Contact {
 	}
 	@Override
 	public String toString() {
-		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", phoneNumber="
-				+ phoneNumber + ", emailID=" + emailID + ", city=" + city + ", state=" + state + ", zip=" + zip + "]";
+		return "Contact [\n firstName=" + firstName + ",\n lastName=" + lastName + ",\n address=" + address + ",\n phoneNumber="
+				+ phoneNumber + ",\n emailID=" + emailID + ",\n city=" + city + ",\n state=" + state + ",\n zip=" + zip + "]";
 	}
 }
